55908721f2a0f8ff3d07e47f461292f44aa94fda
J Zim <zimjavajob@gmail.com>
2016-08-03 17:28:21 +0500

    create class Student

---
+++ src/com/java/datastructure/Hexlet/module/m03e02/Student.java
@@ -0,0 +1,13 @@
+package com.java.datastructure.Hexlet.module.m03e02;
+
+public class Student {
+
+    private final String uuid;
+
+    public Student(final String uuid) {
+        this.uuid = uuid;
+    }
+
+
+
+}
